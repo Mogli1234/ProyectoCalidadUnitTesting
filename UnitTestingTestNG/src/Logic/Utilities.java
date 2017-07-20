8a58727b7b018f91ecbd1a142bdc283160ff0dfc
Jaime A. Esquivel <jaimeesquivelgomez@gmail.com>
2017-07-19 19:42:59 -0600

    structure for the project of unitTesting using TestNg

---
+++ UnitTestingTestNG/src/Logic/Utilities.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author MOGLI
+ */
+public class Utilities {
+    
+    //this is mine
+    public int divide(int num1, int num2){
+        return 0;
+    }
+    
+    
+    
+    //This is you function pablo 
+    public int xtractNumberForString(){
+        return 0;
+    }
+}
