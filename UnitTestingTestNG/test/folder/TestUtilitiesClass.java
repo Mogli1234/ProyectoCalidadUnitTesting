1879a7e554e82c73ac8bfcdfc4b93e5bdfb4aa9b
Jaime A. Esquivel <jaimeesquivelgomez@gmail.com>
2017-07-23 14:28:19 -0600

    Fixed the problem with the xml file

---
+++ UnitTestingTestNG/test/folder/TestUtilitiesClass.java
@@ -0,0 +1,29 @@
+package folder;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Logic.Utilities;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author MOGLI
+ */
+public class TestUtilitiesClass {
+    Utilities util = new Utilities();
+    
+    public TestUtilitiesClass() {
+    }
+
+    @Parameters({"Number1","Number2"})
+    @Test
+    public void TestDivider(int Number1, int Number2){
+    util.divide(Number1, Number2);
+    }
+        
+}
