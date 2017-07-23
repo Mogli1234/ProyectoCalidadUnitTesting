98f82e3f2e7ddc676b82518b3d0ed0df382cc59a
PABLOSANCHEZSOTO <J_pablo_s_s@hotmail.com>
2017-07-23 15:14:24 -0600

    add test case String on the xml

--- UnitTestingTestNG/test/folder/TestNumericString.java
+++ UnitTestingTestNG/test/folder/TestNumericString.java
@@ -1,30 +1,27 @@
-package folder;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package folder;
 
 import Logic.Utilities;
+import static org.testng.Assert.*;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
  *
- * @author MOGLI
+ * @author Josepablo
  */
-public class TestUtilitiesClass {
-    Utilities util = new Utilities();
-    
-    public TestUtilitiesClass() {
+public class TestNumericString {
+       Utilities util = new Utilities();
+    public TestNumericString() {
     }
-
-    @Parameters({"Number1","Number2"})
+ @Parameters({"cadena"})
     @Test
-    public void TestDivider(int Number1, int Number2){
-    util.divide(Number1, Number2);
-    }
+    public void TestXtractNumberForString(String cadena){
+    util.xtractNumberForString(cadena);
+    } 
     
-   
 }
